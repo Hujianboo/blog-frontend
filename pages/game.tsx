e1aaed86d35b36bd5e359a7ccbc607e4188436fc
hujianbo <466636709@qq.com>
2023-07-24 17:58:57 +0800

    feat: 更改样式

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,32 +1,23 @@
-import Head from 'next/head'
-import SideBar from '../components/SideBar'
-import Layout from '../components/Layout'
-import { ReactElement } from 'react'
-import List from '../components/List'
-import { getAllPosts } from '../utils'
+import Head from "next/head";
+import SideBar from "../components/SideBar";
+import Layout from "../components/Layout";
+import { ReactElement } from "react";
+import List from "../components/List";
+import { getAllPosts } from "../utils";
 // import './style.scss'
-export default function Game({allPosts}) {
-  return (
-    <List allPosts={allPosts}></List>
-  )
+export default function Game({ allPosts }) {
+  return allPosts.length !== 0 && <List allPosts={allPosts}></List>;
 }
 Game.getLayout = function getLayout(page: ReactElement) {
-  return (
-      <Layout>
-        {page}
-      </Layout>
-
-  )
-}
+  return <Layout>{page}</Layout>;
+};
 
 export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    'title',
-    'tag',
-    'date'
-  ]).filter((item) => item.tag === 'game')
-  
+  const allPosts = getAllPosts(["title", "tag", "date"]).filter(
+    (item) => item.tag === "game"
+  );
+
   return {
     props: { allPosts },
-  }
+  };
 }
