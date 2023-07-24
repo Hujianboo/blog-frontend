d1cf70b33cc47c08b7b0e95197ccbf84126f6090
hujianbo <466636709@qq.com>
2023-07-24 18:42:51 +0800

    feat: change style

--- components/SideBar/index.tsx
+++ components/SideBar/index.tsx
@@ -1,53 +1,53 @@
-import Link from 'next/link'
-import classnames from 'classnames'
+import Link from "next/link";
+import classnames from "classnames";
 export default function SideBar({}) {
   return (
-      <div className="side-bar">
-        <Link href={"/"}>
-          <div
-            className={classnames({
-              "flip-container": true,
-            })}
-          >
-            <div className="flipper">
-              <div className="front">
-                <img
-                  className="avatar"
-                  src={'/avatar-front.png'}
-                  alt=""
-                  width={120}
-                  height={120}
-                />
-              </div>
-              <div className="back">
-                <img
-                  className="avatar"
-                  src={'/avatar-back.png'}
-                  alt=""
-                  width={120}
-                  height={120}
-                />
-              </div>
+    <div className="side-bar">
+      <Link href={"/"}>
+        <div
+          className={classnames({
+            "flip-container": true,
+          })}
+        >
+          <div className="flipper">
+            <div className="front">
+              <img
+                className="avatar"
+                src={"/avatar-front.png"}
+                alt=""
+                width={120}
+                height={120}
+              />
+            </div>
+            <div className="back">
+              <img
+                className="avatar"
+                src={"/avatar-back.png"}
+                alt=""
+                width={120}
+                height={120}
+              />
             </div>
           </div>
-        </Link>
-        <div className="nav">
-          <Link href={"/tech"}>
-            <div className="tag">技术</div>
-          </Link>
-          <Link href={"/game"}>
-            <div className="tag">游戏</div>
-          </Link>
-          <Link href={"/reading"}>
-            <div className="tag">阅读</div>
-          </Link>
-          <Link href={"/life"}>
-            <div className="tag">生活</div>
-          </Link>
-          <Link href={"/about"} >
-            <div className="tag">关于我</div>
-          </Link>
         </div>
+      </Link>
+      <div className="nav">
+        <Link href={"/tech"}>
+          <div className="tag">技术</div>
+        </Link>
+        <Link href={"/game"}>
+          <div className="tag">游戏</div>
+        </Link>
+        <Link href={"/reading"}>
+          <div className="tag">阅读</div>
+        </Link>
+        <Link href={"/life"}>
+          <div className="tag">生活</div>
+        </Link>
+        <Link href={"/about"}>
+          <div className="tag">关于我</div>
+        </Link>
       </div>
-  )
+    </div>
+  );
 }
