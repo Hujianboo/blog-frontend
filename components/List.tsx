0874a3ca714e45e8f3b94b63362af008f3845edc
hujianbo <466636709@qq.com>
2023-07-24 22:19:23 +0800

    feat: add

--- components/List.tsx
+++ components/List.tsx
@@ -1,19 +1,17 @@
-import Link from 'next/link'
-export default function List({allPosts}) {
+import Link from "next/link";
+export default function List({ allPosts }) {
   return (
     <div className="article-list">
-      {allPosts &&
-        allPosts.map((item, index) => (
-          <Link href={`/posts/${item.title}`}>
-          <div
-            key={index}
-            className="article-list-tab"
-          >
-            {item.title}
-          </div>
-          </Link>
-          
-        ))}
+      <figcaption>
+        {allPosts &&
+          allPosts.map((item, index) => (
+            <Link href={`/posts/${item.title}`}>
+              <div key={index} className="article-list-tab">
+                {item.title}
+              </div>
+            </Link>
+          ))}
+      </figcaption>
     </div>
-  )
+  );
 }
