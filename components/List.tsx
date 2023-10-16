76ebee4439f01bf57dad0e80635726bbdda78c1c
hujianbo <466636709@qq.com>
2023-10-16 17:24:48 +0800

    feat: add

--- components/List.tsx
+++ components/List.tsx
@@ -1,29 +1,38 @@
 import Link from "next/link";
-export default function List({ allPosts, disabled }) {
-  return (
-    <div className="article-list">
-      <figcaption>
-        {allPosts &&
-          allPosts.map((item, index) => {
-            if (disabled) {
-              return (
-                <a className="article-list-href-disabled">
-                  <div key={index} className="article-list-tab">
-                    {item.title}
-                  </div>
-                </a>
-              );
-            } else {
-              return (
-                <Link href={`/posts/${item.title}`}>
-                  <div key={index} className="article-list-tab">
-                    {item.title}
-                  </div>
-                </Link>
-              );
-            }
-          })}
-      </figcaption>
-    </div>
-  );
+import React from "react";
+
+interface ListProps {
+  allPosts: any;
+  disabled?: boolean;
 }
+const List: React.FC<ListProps> = ({ allPosts, disabled }) => {
+  {
+    return (
+      <div className="article-list">
+        <figcaption>
+          {allPosts &&
+            allPosts.map((item, index) => {
+              if (disabled) {
+                return (
+                  <a className="article-list-href-disabled">
+                    <div key={index} className="article-list-tab">
+                      {item.title}
+                    </div>
+                  </a>
+                );
+              } else {
+                return (
+                  <Link href={`/posts/${item.title}`}>
+                    <div key={index} className="article-list-tab">
+                      {item.title}
+                    </div>
+                  </Link>
+                );
+              }
+            })}
+        </figcaption>
+      </div>
+    );
+  }
+};
+export default List;
