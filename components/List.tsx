3e3153a09a022ffff71bdd8f4d01b9ba3689ad97
hujianbo <466636709@qq.com>
2023-10-16 14:34:54 +0800

    feat: 隐藏

--- components/List.tsx
+++ components/List.tsx
@@ -1,16 +1,28 @@
 import Link from "next/link";
-export default function List({ allPosts }) {
+export default function List({ allPosts, disabled }) {
   return (
     <div className="article-list">
       <figcaption>
         {allPosts &&
-          allPosts.map((item, index) => (
-            <Link href={`/posts/${item.title}`}>
-              <div key={index} className="article-list-tab">
-                {item.title}
-              </div>
-            </Link>
-          ))}
+          allPosts.map((item, index) => {
+            if (disabled) {
+              return (
+                <a className="article-list-href-disabled">
+                  <div key={index} className="article-list-tab">
+                    {item.title}
+                  </div>
+                </a>
+              );
+            } else {
+              return (
+                <Link href={`/posts/${item.title}`}>
+                  <div key={index} className="article-list-tab">
+                    {item.title}
+                  </div>
+                </Link>
+              );
+            }
+          })}
       </figcaption>
     </div>
   );
--- pages/life.tsx
+++ pages/life.tsx
@@ -1,31 +1,23 @@
-import Head from 'next/head'
-import SideBar from '../components/SideBar'
-import Layout from '../components/Layout'
-import { ReactElement } from 'react'
-import { getAllPosts } from '../utils'
-import List from '../components/List'
+import Head from "next/head";
+import SideBar from "../components/SideBar";
+import Layout from "../components/Layout";
+import { ReactElement } from "react";
+import { getAllPosts } from "../utils";
+import List from "../components/List";
+import { appendFile } from "fs";
 // import './style.scss'
-export default function Life({allPosts}) {
-  return (
-    <List allPosts={allPosts}></List>
-  )
+export default function Life({ allPosts }) {
+  return <List allPosts={allPosts} disabled={true}></List>;
 }
 Life.getLayout = function getLayout(page: ReactElement) {
-  return (
-    <Layout>
-      {page}
-    </Layout>
-  )
-}
+  return <Layout>{page}</Layout>;
+};
 
 export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    'title',
-    'tag',
-    'date'
-  ]).filter((item) => item.tag === 'life')
-  
+  const allPosts = getAllPosts(["title", "tag", "date"]).filter(
+    (item) => item.tag === "life"
+  );
   return {
     props: { allPosts },
-  }
+  };
 }
